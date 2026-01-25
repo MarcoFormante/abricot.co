6c309f5af1d30b73b37e9414fdb1f7bd6462ec66
Marco <formante.marco@gmail.com>
2026-01-25 22:45:50 +0000

    same page component for login and register page

---
+++ frontend/app/components/SignInUp/SignInUp.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { Logo } from "../Logo/Logo";
+import { ConnectionForm } from "../ConnectionForm/ConnectionForm";
+import signinImg from "../../assets/Signin.webp"
+import Link from "next/link";
+
+export function SignInUp({isRegisterPage}:{
+    isRegisterPage:boolean
+}){
+
+    return (
+         <div className="flex min-h-screen w-full">
+          <div className="w-140.5 flex flex-col justify-center items-center gap-50.5">
+              <div>
+                <Logo fill={"#D3590B"}/>
+              </div>
+              <div>
+                 <ConnectionForm isRegisterPage={isRegisterPage}/>
+              </div>
+              <div>
+                <p className="text-[14px]">
+                    {isRegisterPage ? "Déjà inscrit ?" : "Pas encore de compte ?"}
+                    <Link 
+                        className="text-[#D3590B] underline underline-offset-3 ml-2" 
+                        href={isRegisterPage ? "/" :  "/inscription"}>
+                        {isRegisterPage ? "Se connecter" :" Créer un compte" }
+                    </Link></p> 
+              </div>
+          </div>
+          <div className="max-w-219.5">
+              <Image 
+                src={signinImg} 
+                alt="Abricot" 
+                className="object-cover h-full object-right"
+              />
+          </div>
+      </div>
+    )
+}
