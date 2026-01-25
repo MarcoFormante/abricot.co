4245ef8b6939f50b6a311a44823d6440679a51ea
Marco <formante.marco@gmail.com>
2026-01-25 22:43:01 +0000

    create connectionForm component for both pages (register & login)

---
+++ frontend/app/components/ConnectionForm/ConnectionForm.tsx
@@ -0,0 +1,17 @@
+import { Button } from "../Button/Button";
+import { Form } from "./Form/Form";
+
+export function ConnectionForm({isRegisterPage}:{
+    isRegisterPage:boolean
+}){
+    return (
+        <div className="w-70.5 flex flex-col gap-7.5">
+            <h1 className="text-[#D3590B] font-bold text-[40px] text-center">{isRegisterPage ? "Inscription" : "Connexion"}</h1>
+            <div>
+                <Form>
+                    <Button label={isRegisterPage ?"S’inscrire": "Se connecter"} type="btn-softBlack"/>
+                </Form>
+            </div>
+        </div>
+    )
+}
