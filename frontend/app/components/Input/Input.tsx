59b71c1c672b9c1dea7547c5a17c6747e38580ba
Marco <formante.marco@gmail.com>
2026-01-28 14:56:25 +0000

    create input component to handle text,password and search inputs (for the moment)

--- frontend/app/components/Input/Input.tsx
+++ frontend/app/components/Input/Input.tsx
@@ -1,12 +1,13 @@
-export function Input({label,type,name}:{
-    label:string,
-    type: "text" | "password",
-    name:string
+export function Input({label,type,name,placeholder}:{
+    label?:string,
+    type: "text" | "password" | "search",
+    name:string,
+    placeholder:string
 }){
     return(
-        <div className="flex flex-col">
+        <div className="flex flex-col w-full">
             <label htmlFor={name} className="font-normal text-[14px]">{label}</label>
-            <input type={type} className="h-13.25 rounded-sm bg-[#FFFFFF] border border-[#E5E7EB] pl-1.5" name={name} id={name} />
+            <input placeholder={placeholder} type={type} className={`h-13.25 rounded-sm bg-[#FFFFFF] border border-[#E5E7EB] pl-1.5 w-full ${type === "search" ? "pl-[32px] text-[14px] h-[63px]" : ""}`} name={name} id={name} />
         </div>
-    )
+    )   
 }
