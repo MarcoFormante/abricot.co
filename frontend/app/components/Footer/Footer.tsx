83fd91b6cca354a0f3636e124cd61372c47ba543
Marco <formante.marco@gmail.com>
2026-01-29 18:20:10 +0000

    create Footer with logo and copyright

--- frontend/app/components/Footer/Footer.tsx
+++ frontend/app/components/Footer/Footer.tsx
@@ -1,7 +1,13 @@
+import { Logo } from "../Logo/Logo";
+
 export function Footer(){
+    const year = new Date().getFullYear()
     return (
-        <footer>
-            footer
+        <footer className="h-[68px] bg-[#FFFFFF] mt-[48px] fixed bottom-0 left-[50%] translate-x-[-50%] w-full max-w-[1440px] m-auto">
+            <div className="flex justify-between items-center h-full pl-[30px] pr-[54px]">
+                <Logo fill={"#0F0F0F"} width={"101"} height={"12.86"}/>
+                <p>Abricot {year}</p>
+            </div>
         </footer>
     )
 }
