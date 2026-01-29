1e29c35a501fa1c77d6b905d64bfc2aead018de9
Marco <formante.marco@gmail.com>
2026-01-29 18:13:53 +0000

    add font size 14px

--- frontend/app/components/Dashboard/Tasks/TaskKanban.tsx
+++ frontend/app/components/Dashboard/Tasks/TaskKanban.tsx
@@ -6,7 +6,7 @@ export function TaskKanban(){
             <div className="bg-[#FFFFFF] border border-[#FFE0E0] w-[419px] min-h-[30vh] pt-[40px] px-[24px] rounded-[10px]">
                 <div>
                     <h3 className="text-[18px] font-semibold max-w-max inline">À faire</h3>
-                    <span className="bg-[#E5E7EB] text-[#6B7280] py-[4px] px-[16px] w-[41px] h-[25px] rounded-[50px] ml-[8px]">4</span>
+                    <span className="bg-[#E5E7EB] text-[#6B7280] py-[4px] px-[16px] w-[41px] h-[25px] rounded-[50px] ml-[8px] text-[14px]">4</span>
                 </div>
                 <ul className="mt-[41px] flex flex-col gap-[16px] pb-[40px]">
                     <KanbanItem tagType={"to-do"}/>
@@ -18,7 +18,7 @@ export function TaskKanban(){
             <div className="bg-[#FFFFFF] border border-[#FFE0E0] w-[419px] min-h-[30vh] pt-[40px] px-[24px] rounded-[10px]">
                 <div>
                     <h3 className="text-[18px] font-semibold max-w-max inline">En cours</h3>
-                    <span className="bg-[#E5E7EB] text-[#6B7280] py-[4px] px-[16px] w-[41px] h-[25px] rounded-[50px] ml-[8px]">3</span>
+                    <span className="bg-[#E5E7EB] text-[#6B7280] py-[4px] px-[16px] w-[41px] h-[25px] rounded-[50px] ml-[8px] text-[14px]">3</span>
                 </div>
                 <ul className="mt-[41px] flex flex-col gap-[16px] pb-[40px]">
                     <KanbanItem tagType={"in-progress"}/>
@@ -30,7 +30,7 @@ export function TaskKanban(){
              <div className="bg-[#FFFFFF] border border-[#FFE0E0] w-[419px] min-h-[30vh] pt-[40px] px-[24px] rounded-[10px]">
                 <div>
                     <h3 className="text-[18px] font-semibold max-w-max inline">Terminées</h3>
-                    <span className="bg-[#E5E7EB] text-[#6B7280] py-[4px] px-[16px] w-[41px] h-[25px] rounded-[50px] ml-[8px]">3</span>
+                    <span className="bg-[#E5E7EB] text-[#6B7280] py-[4px] px-[16px] w-[41px] h-[25px] rounded-[50px] ml-[8px] text-[14px]">3</span>
                 </div>
                 <ul className="mt-[41px] flex flex-col gap-[16px] pb-[40px]">
                     <KanbanItem tagType={"done"}/>
