d157169ed805b1b32fd977218547cf63352b7c59
Marco <formante.marco@gmail.com>
2026-01-29 19:37:46 +0000

    create  nametag component for collaborators section

---
+++ frontend/app/components/Collaborators/NameTag.tsx
@@ -0,0 +1,13 @@
+export function NameTag({name,isOwner}:{
+    name?:string,
+    isOwner:boolean,
+    
+}){
+
+
+    return (
+        <div className={` capitalize ml-[5px] mr-[4px] bg-[${isOwner === true ? "#FFE8D9" : "#E5E7EB"}] text-[14px] text-[${isOwner === true ? "#EF4444" : "#6B7280"}] min-w-[109px] h-[25px] rounded-[50px] flex justify-center items-center px-[16px]`}>
+            {isOwner ? "Propriétaire" : name}
+        </div>
+    )
+}
