464760b49b2821c81721642ed668ba2201ab3e16
Marco <formante.marco@gmail.com>
2026-01-31 11:57:36 +0000

    add gap, required , type date in props

--- frontend/app/components/Input/Input.tsx
+++ frontend/app/components/Input/Input.tsx
@@ -1,13 +1,15 @@
-export function Input({label,type,name,placeholder}:{
+export function Input({label,type,name,placeholder,gap,required}:{
     label?:string,
-    type: "text" | "password" | "search",
+    type: "text" | "password" | "search" | "date",
     name:string,
-    placeholder:string
+    placeholder?:string,
+    gap?:string,
+    required?:boolean
 }){
     return(
-        <div className="flex flex-col w-full">
-            <label htmlFor={name} className="font-normal text-[14px]">{label}</label>
-            <input placeholder={placeholder} type={type} className={`h-13.25 rounded-sm bg-[#FFFFFF] border border-[#E5E7EB] pl-1.5 w-full ${type === "search" ? "pl-[32px] text-[14px] h-[63px]" : ""}`} name={name} id={name} />
+        <div className={`flex flex-col w-full gap-[${gap}]`}>
+            <label hidden={!label} htmlFor={name} className="font-normal text-[14px]">{label || placeholder}</label>
+            <input required={required} placeholder={placeholder} id={name} name={name} type={type} className={`h-13.25 rounded-sm bg-[#FFFFFF] border border-[#E5E7EB] pl-1.5 w-full ${type === "search" ? "pl-[32px] text-[14px] h-[63px] " : ""}`} />
         </div>
     )   
 }
