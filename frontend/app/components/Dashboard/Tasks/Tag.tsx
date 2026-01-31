ae0c20c3d81d1506667f0e2112848759a300600e
Marco <formante.marco@gmail.com>
2026-01-31 11:56:19 +0000

    add classname to handle style in scss file

--- frontend/app/components/Dashboard/Tasks/Tag.tsx
+++ frontend/app/components/Dashboard/Tasks/Tag.tsx
@@ -2,7 +2,7 @@ export function Tag({type}:{
     type:"to-do" | "in-progress" | "done"
 }){
     return (
-        <span className={`${type === "to-do" ? "text-[#EF4444] bg-[#FFE0E0]" : type === "in-progress" ? "text-[#E08D00] bg-[#FFF0D7]" : "text-[#27AE60] bg-[#F1FFF7]"}  w-[75px] min-w-[75px] text-center pt-[4px] pb-[2px]  rounded-[50px] text-[14px] block`}>
+        <span className={`${type === "to-do" ? "text-[#EF4444] bg-[#FFE0E0]" : type === "in-progress" ? "text-[#E08D00] bg-[#FFF0D7]" : "text-[#27AE60] bg-[#F1FFF7]"} task-tag  w-[75px] min-w-[75px] text-center pt-[4px] pb-[2px]  rounded-[50px] text-[14px] block`}>
             {
                 type === "to-do" ? "À faire" : type === "in-progress" ? "En cours" : "Terminée"
             }
