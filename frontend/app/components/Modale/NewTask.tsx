2f2ddf9faf9f9198063bc40bc4b39a92f9fc5862
Marco <formante.marco@gmail.com>
2026-01-31 12:09:05 +0000

    modale to handle new project by a form

---
+++ frontend/app/components/Modale/NewTask.tsx
@@ -0,0 +1,75 @@
+import { Input } from '../Input/Input';
+import { Tag } from '../Dashboard/Tasks/Tag';
+import { Button } from '../Button/Button';
+import { FormEvent, FormEventHandler } from 'react';
+
+
+export function NewTask(){
+
+    function onSubmit(event: React.FormEvent) {
+        event.preventDefault()
+        const formData = new FormData(event.target as HTMLFormElement)
+        const title = formData.get('title')
+        const desc = formData.get('desc')
+        const date = formData.get('date')
+
+        if (!title && !desc && !date) {
+            return
+        }
+
+        const formValues = {
+            title: formData.get('title'),
+            desc: formData.get('desc'),
+            date: formData.get('date'),
+            collabs: formData.get('collaborators'),
+            status: formData.get('status[]')
+        }
+
+        console.log(formValues);
+        
+
+    }
+
+    return (
+        <div className="flex flex-col gap-[40px]">
+            <h5 className="font-semibold text-[24px] text-[#1F1F1F]">Créer une tâche</h5>
+            <form onSubmit={onSubmit} className='flex flex-col gap-[24px]'>
+                <Input type='text' name='title' label='Title*' gap='6px' required/>
+                <Input type='text' name='desc' label='Description*' gap='6px' required/>
+                <Input type={"date"} name='date' label='Échéance*' required/>
+                
+                <div>
+                    <label htmlFor="collaborators" className='text-[14px]'>Assigné à :</label>
+                    <select name="collaborators" id="collaborators" className="select-container h-[53px]  pl-[6px] w-full text-[14px]  rounded-sm bg-[#FFFFFF] border border-[#E5E7EB]  pl-1.5 text-[#6B7280]">
+                        <option value="" className="text-[#6B7280]">Choisir un ou plusieurs collaborateurs</option>
+                        <option value="" className="text-[#6B7280]"></option>
+                    </select>
+                </div>
+
+                <div>
+                    <span className='text-[14px]'>Statut :</span>
+                    <div className='flex items-center gap-[8px] mt-[15px]'>
+                        <input type="radio" name="status[]" id="to-do" value={"to-do"} />
+                        <label htmlFor="to-do">
+                            <Tag type='to-do'/>
+                        </label>
+                        
+                        <input type="radio" name="status[]" id="in-progress" value={"in-progress"} />
+                        <label htmlFor="in-progress">
+                            <Tag type='in-progress'/>
+                        </label>
+
+                        <input type="radio" name="status[]" id="done" value={"done"} />
+                        <label htmlFor="done">
+                            <Tag type='done'/>
+                        </label>
+                    </div>
+                </div>
+
+                <div className='w-[181px] h-[50px] mt-[32px]'>
+                    <Button type={"btn-grey"} label={"+ Ajouter une tâche"} />
+                </div>
+            </form>
+        </div>
+    )
+}
