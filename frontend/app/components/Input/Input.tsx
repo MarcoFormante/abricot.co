95a9c9d9844663c02903443fa444846c1d03ece9
Marco <formante.marco@gmail.com>
2026-02-02 14:18:09 +0000

    minor updates in input component

--- frontend/app/components/Input/Input.tsx
+++ frontend/app/components/Input/Input.tsx
@@ -2,7 +2,7 @@ import { ChangeEventHandler } from "react"
 
 export function Input({label,type,name,placeholder,gap,required,onChange,value}:{
     label?:string,
-    type: "text" | "password" | "search" | "date",
+    type: "text" | "password" | "search" | "date" | "email",
     name:string,
     placeholder?:string,
     gap?:string,
@@ -13,7 +13,7 @@ export function Input({label,type,name,placeholder,gap,required,onChange,value}:
     return(
         <div className={`flex flex-col w-full gap-[${gap}]`}>
             <label hidden={!label} htmlFor={name} className="font-normal text-[14px]">{label || placeholder}</label>
-            <input onChange={onChange} required={required} placeholder={placeholder} id={name} name={name} type={type} value={value ?? ""} className={`h-13.25 pl-[17px] rounded-sm bg-[#FFFFFF] border border-[#E5E7EB] pl-1.5 w-full ${type === "search" ? "pl-[32px] text-[14px] h-[63px] " : ""}`} />
+            <input onChange={onChange} required={required} placeholder={placeholder} id={name} name={name} type={type} value={value ?? ""} className={`h-13.25 pl-[17px] rounded-sm bg-[#FFFFFF] border border-[#E5E7EB] pl-1.5 w-full text-[#6B7280] ${type === "search" ? "pl-[32px] text-[14px] h-[63px] " : ""}`} />
         </div>
     )   
 }
