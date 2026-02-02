7af8cf4ae7003f6d46e8503316ed67a2f43a907e
Marco <formante.marco@gmail.com>
2026-02-02 14:17:06 +0000

    add style to profile button, and add pathname in parent component Header

--- frontend/app/components/Header/Header.tsx
+++ frontend/app/components/Header/Header.tsx
@@ -1,10 +1,15 @@
+'use client'
 import Link from "next/link";
 import { Logo } from "../Logo/Logo";
 import styles from "./Header.module.scss";
 import { MenuItem } from "./MenuItem";
+import { usePathname } from "next/navigation";
 
-export async function Header(){
+export function Header(){
+    const pathname = usePathname()
+    console.log(pathname === "/mon-compte");
     
+
     return (
         <header className="bg-[#FFFFFF] px-25">
             <nav>
@@ -23,16 +28,18 @@ export async function Header(){
                            label="Tableau de bord"
                            imgClass="svgTbord"
                            href="/dashboard"
+                           pathname={pathname}
                         />
                     
                         <MenuItem 
                            label="Projets"
                            imgClass="svgProjets"
                            href="/projets"
+                           pathname={pathname}
                         />
                     </li>
                     <li>
-                        <Link href={"/mon-compte"}>
+                        <Link href={"/mon-compte"} className={`w-[65px] h-[65px]  block flex justify-center items-center rounded-[32.5px] text-[14px] text-[#0F0F0F] ${pathname === "/mon-compte" ? "bg-[#D3590B] text-[#FFFFFF]" : "bg-[#FFE8D9]"}`}>
                             AD
                         </Link>
                     </li>
