a9f83da21257df6773fa48d04807980f8626a53d
Marco <formante.marco@gmail.com>
2026-02-06 19:15:59 +0000

    get projects and add data to project components

--- frontend/app/(admin)/dashboard/page.tsx
+++ frontend/app/(admin)/dashboard/page.tsx
@@ -1,33 +1,30 @@
-'use client'
 import { AdminMainTitle } from "@/app/components/Dashboard/AdminMainTitle/AdminMainTitle";
 import { Button } from "@/app/components/Button/Button";
-import { SwitchButton } from "@/app/components/Button/SwitchButton";
-import { useState } from "react";
-import { TaskList } from "@/app/components/Dashboard/Tasks/TaskList";
-import { TaskKanban } from "@/app/components/Dashboard/Tasks/TaskKanban";
 
-export default function Dashboard() {
-    const [isList,setIsList] = useState(true)
+import TasksSection from "@/app/components/Dashboard/Tasks/TasksSection";
+import getDashboardTasks from "@/app/actions/dashboard";
+import { cookies } from "next/headers";
 
+export default async function Dashboard() {
+
+    const userInfo =(await cookies()).get("user_info")?.value
+    const userParsed = JSON.parse(userInfo as string)
+
+    const response = await getDashboardTasks()
+    const tasksResponse = await response.data
+    
     return (
         <main className="pt-[89px] px-25 pb-[78px]">
             <div className="flex justify-between">
-                <AdminMainTitle title={"Tableau de bord"} text="Bonjour Alice Dupont, voici un aperçu de vos projets et tâches"/>
+                <AdminMainTitle title={"Tableau de bord"} text={`Bonjour ${userParsed.name}, voici un aperçu de vos projets et tâches`}/>
                 <div className="h-[50px] self-end">
                     <div className="w-[181px] h-[50px]">
                         <Button type={"btn-black"} label="+ Créer un projet"/>
                     </div>
                 </div>
             </div>
-            
-            <div className="flex items-center gap-[10px] mt-[60px]">
-                <SwitchButton svgName={"listeSvg"} label={"Liste"} isActive={isList} onClick={()=>setIsList(true)}/>
-                <SwitchButton svgName={"kanbanSvg"} label={"Kanban"} isActive={!isList} onClick={()=>setIsList(false)}/>
-            </div>
 
-
-            {isList ? <TaskList/> : <TaskKanban/> }
-
+           <TasksSection tasks={tasksResponse?.data?.tasks || []}/>
         </main>
     )
 }
