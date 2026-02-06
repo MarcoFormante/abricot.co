463ce79798dae079a19cf5ef6106c65a182b5147
Marco <formante.marco@gmail.com>
2026-02-06 19:47:51 +0000

    change input type to email, handle onSubmit in parent component

--- frontend/app/components/ConnectionForm/Form/Form.tsx
+++ frontend/app/components/ConnectionForm/Form/Form.tsx
@@ -1,12 +1,16 @@
-import React from "react";
 import { Input } from "../../Input/Input";
 
-export function Form({children}:{children:React.ReactNode}){
+export function Form({ children,onSubmit }: 
+    { 
+        children: React.ReactNode 
+        onSubmit:React.FocusEventHandler<HTMLFormElement>
+    }){
+
     return (
-        <form className="flex flex-col gap-7.25" method="POST">
-            <Input label="Email" type="text" name="email"/>
-            <Input label="Mot de passe" type="password" name="password"/>
-            <div className="h-12.5 px-[32.5px]">
+        <form onSubmit={onSubmit} className="flex flex-col gap-7.25">
+            <Input label="Email" type="email" name="email" required />
+            <Input label="Mot de passe" type="password" name="password" required />
+            <div className={`h-12.5 px-[32.5px]`}>
                 {children}
             </div>
         </form>
