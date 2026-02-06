2f739a1091490d111587df493ec950968c2eccac
Marco <formante.marco@gmail.com>
2026-02-06 19:25:16 +0000

    add a component for every section in project page and add props data

--- frontend/app/(admin)/projets/[id]/page.tsx
+++ frontend/app/(admin)/projets/[id]/page.tsx
@@ -1,19 +1,26 @@
 import { ProjectHeader } from "@/app/components/SingleProject/ProjectHeader";
 import { ProjectContributors } from "@/app/components/SingleProject/ProjectContributors";
 import { ProjectTasks } from "@/app/components/SingleProject/ProjectTasks/ProjectTasks";
+import {getProjectByID} from "@/app/actions/project";
 
 export default async function SingleProject({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
-  const param = await params;
 
+  const param = await params;
+  const project = await getProjectByID(param.id)
+  
   return (
     <main className="pb-[78px]">
-      <ProjectHeader/> 
-      <ProjectContributors/>
-      <ProjectTasks/>
+    {  project && 
+      <>
+        <ProjectHeader id={param.id} name={project.data.name} description={project.data.description}/> 
+        <ProjectContributors ownerName={project.data.owner.name} members={project.data.members}/>
+        <ProjectTasks tasks={project.tasks}/>
+       </>
+    }
     </main>
   );
 }
