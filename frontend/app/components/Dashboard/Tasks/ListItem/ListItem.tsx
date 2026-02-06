463b8d397756d3f93940ca96089a03377f989d4c
Marco <formante.marco@gmail.com>
2026-02-06 19:44:42 +0000

    add tasks data into the html, add href int the button Component handle new route

--- frontend/app/components/Dashboard/Tasks/ListItem/ListItem.tsx
+++ frontend/app/components/Dashboard/Tasks/ListItem/ListItem.tsx
@@ -2,25 +2,29 @@ import { Button } from "../../../Button/Button";
 import { Tag } from "../Tag";
 import { TaskItemProjectDetails } from "../TaskItemProjectDetails";
 
-export function ListItem({tagType}:{
-    tagType:"to-do" | "in-progress" | "done"
-}) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function ListItem({task}:{task:any}) {
+
   return (
     <li className="border border-[#E5E7EB] rounded-[10px] bg-[#FFFFFF] pt-[32.23px] h-[162px] px-[40px]">
       <div className="flex justify-between h-[112px]">
         <div className="flex flex-col gap-[32px]">
           <div className="flex flex-col gap-[7px]">
-            <h3 className="text-[18px] font-semibold">Nom de la tâche</h3>
+            <h3 className="text-[18px] font-semibold">{task.title}</h3>
             <p className="text-[14px] text-[#6B7280]">
-              Description de la tâche
+              {task.description}
             </p>
           </div>
-           <TaskItemProjectDetails/>
+           <TaskItemProjectDetails 
+              projectName={task.project.name} 
+              dueDate={new Date(task.dueDate)}
+              commentsLength={task.comments.length}
+           />
         </div>
         <div className="flex flex-col justify-between items-end">
-          <Tag type={tagType} />
+          <Tag type={task.status} />
           <div className="w-[121px] h-[50px]">
-            <Button type={"btn-softBlack"} label="Voir" />
+            <Button type={"btn-softBlack"} label="Voir" isLink={true} href={"/projets/" + task.projectId} />
           </div>
         </div>
       </div>
