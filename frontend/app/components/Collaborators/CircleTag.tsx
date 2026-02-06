d0b41ff9574c063b30bfb4ac6243d949a206684b
Marco <formante.marco@gmail.com>
2026-02-06 19:31:25 +0000

    add props,classname to handle the right value when is position 'relative' into the project component

--- frontend/app/components/Collaborators/CircleTag.tsx
+++ frontend/app/components/Collaborators/CircleTag.tsx
@@ -1,10 +1,12 @@
-export function CircleTag({name,isOwner}:{
+export function CircleTag({name,isOwner,classname,rightValue}:{
     name?:string,
     isOwner:boolean,
+    classname?:string,
+    rightValue?:number
 }){
  
     return (
-        <span className={`uppercase rounded-full bg-[${isOwner === true ? "#FFE8D9" : "#E5E7EB"}] text-[10px] text-[#0F0F0F] px-[5px] py-[9px] w-[27px] h-[27px] flex justify-center items-center`}>
+        <span data-right={rightValue} className={`circleTag uppercase rounded-full bg-[${isOwner === true ? "#FFE8D9" : "#E5E7EB"}] text-[10px] text-[#0F0F0F] px-[5px] py-[9px] w-[27px] h-[27px] flex justify-center items-center ${classname ?? ""}`}>
             {name?.split(" ").map((w:string) => w.at(0) )}
         </span>
     )
