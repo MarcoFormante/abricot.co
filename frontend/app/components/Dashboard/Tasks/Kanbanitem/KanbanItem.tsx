c26c43e96891dbc7213788c5526a0e0ffdef9006
Marco <formante.marco@gmail.com>
2026-02-06 19:46:51 +0000

    add data into the html

--- frontend/app/components/Dashboard/Tasks/Kanbanitem/KanbanItem.tsx
+++ frontend/app/components/Dashboard/Tasks/Kanbanitem/KanbanItem.tsx
@@ -2,33 +2,37 @@ import { Button } from "../../../Button/Button";
 import { Tag } from "../Tag";
 import { TaskItemProjectDetails } from "../TaskItemProjectDetails";
 
-export function KanbanItem({tagType}:{
-    tagType:"to-do" | "in-progress" | "done"
-}) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function KanbanItem({task}:{task:any}) {
+
   return (
-    <li className="py-[25px] px-[40px] border border-[#E5E7EB] rounded-[10px] flex flex-col gap-[32px] w-[371px]">
+    <li className="py-[25px] px-[20px] border border-[#E5E7EB] rounded-[10px] flex flex-col gap-[32px] w-[371px]">
       <div className="flex justify-between">
         <div className="flex flex-col gap-[1px]">
           <h4 className="text-[18px] font-semibold max-w-max inline">
-            Nom de la tâche
+            {task.title}
           </h4>
-          <p className="text-[14px] text-[#6B7280]">Description de la tâche</p>
+          <p className="text-[14px] text-[#6B7280]">{task.description}</p>
         </div>
         <div>
-          <Tag type={tagType} />
+          <Tag type={task.status} />
         </div>
       </div>
       <div>
         <div>
           <div>
-           <TaskItemProjectDetails/>
+          <TaskItemProjectDetails 
+              projectName={task.project.name} 
+              dueDate={new Date(task.dueDate)}
+              commentsLength={task.comments.length}
+           />
           </div>
         </div>
       </div>
 
       <div>
         <div className="w-[121px] h-[50px]">
-          <Button type={"btn-softBlack"} label="Voir" />
+           <Button type={"btn-softBlack"} label="Voir" isLink={true} href={"/projets/" + task.projectId} />
         </div>
       </div>
     </li>
