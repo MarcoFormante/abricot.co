94f822dbdedaab03d73d71aa906f17f217824d28
Marco <formante.marco@gmail.com>
2026-02-06 19:21:47 +0000

    delete comments

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,24 +1,10 @@
 import { SignInUp } from "./components/SignInUp/SignInUp";
 
-
 export default function Login() {
-
-  //   const req = await fetch("http://localhost:8000/auth/login",{
-  //   method:"POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body:JSON.stringify({
-  //     email: "alice@example.com",
-  //     password: "password123",
-  //   }),
-  // })
-
-  // const res = await req.json()
 
   return (
     <main>
-      <SignInUp isRegisterPage={false}/>
+      <SignInUp isRegisterPage={false} />
     </main>
   );
 }
