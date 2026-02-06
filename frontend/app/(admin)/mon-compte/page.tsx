031d52495cdfce78eee14023f908571343efc573
Marco <formante.marco@gmail.com>
2026-02-06 19:23:47 +0000

    create form component with userData props

--- frontend/app/(admin)/mon-compte/page.tsx
+++ frontend/app/(admin)/mon-compte/page.tsx
@@ -1,25 +1,12 @@
-import { Button } from "@/app/components/Button/Button";
-import { Input } from "@/app/components/Input/Input";
+import { getUserProfile } from "@/app/actions/auth";
+import { UserForm } from "@/app/components/monCompte/UserForm";
 
-export default function MonCompte(){
+export default async function MonCompte(){
+    const profile = await getUserProfile()
+
     return (
         <main className="px-25 pb-[78px]">
-           <div className="mt-[57px] bg-[#FFFFFF] px-[49px] py-[40px] rounded-[10px]">
-                <div>
-                    <h1 className="text-[18px] text-[#1F1F1F] font-semibold">Mon compte</h1>
-                    <p className="mt-[8px] text-[#6B7280]">Amélie Dupont</p>
-                </div>
-                <form className="flex flex-col gap-[24px] mt-[41px]">
-                    <Input name="name" label="Nom" type={"text"} gap={"7px"} value="Dupont" />
-                    <Input name="surname" label="Prénom" type={"text"} gap={"7px"} value="Amelie" />
-                    <Input name="email" label="Email" type={"email"} gap={"7px"} value="a.dupont@mail.com" />
-                    <Input name="password" label="Mot de passe" type={"password"} gap={"7px"} value="58apenjksjabdSj!" />
-
-                    <div className="w-[242px] h-[50px] mt-[17px]">
-                        <Button type={"btn-softBlack"} label="Modifier les informations"/>
-                    </div>
-                </form>
-           </div>
+            <UserForm user={profile.data.user}/>
         </main>
     )
 }
