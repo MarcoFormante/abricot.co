2476619bf08699de3268a265a26f47efcc880907
Marco <formante.marco@gmail.com>
2026-02-06 19:43:03 +0000

    change type of Tags

--- frontend/app/components/Dashboard/Tasks/Tag.tsx
+++ frontend/app/components/Dashboard/Tasks/Tag.tsx
@@ -1,10 +1,10 @@
 export function Tag({type}:{
-    type:"to-do" | "in-progress" | "done"
+    type:"TODO" | "IN_PROGRESS" | "DONE"
 }){
     return (
-        <span className={`${type === "to-do" ? "text-[#EF4444] bg-[#FFE0E0]" : type === "in-progress" ? "text-[#E08D00] bg-[#FFF0D7]" : "text-[#27AE60] bg-[#F1FFF7]"} task-tag  w-[75px] min-w-[75px] text-center pt-[4px] pb-[2px]  rounded-[50px] text-[14px] block`}>
+        <span className={`${type === "TODO" ? "text-[#EF4444] bg-[#FFE0E0]" : type === "IN_PROGRESS" ? "text-[#E08D00] bg-[#FFF0D7]" : "text-[#27AE60] bg-[#F1FFF7]"} task-tag  w-[75px] min-w-[75px] text-center pt-[4px] pb-[2px]  rounded-[50px] text-[14px] block`}>
             {
-                type === "to-do" ? "À faire" : type === "in-progress" ? "En cours" : "Terminée"
+                type === "TODO" ? "À faire" : type === "IN_PROGRESS" ? "En cours" : "Terminée"
             }
         </span>
     )
