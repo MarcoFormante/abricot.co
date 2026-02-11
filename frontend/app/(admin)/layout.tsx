2fe93369ade3ed86eb7b20dbe18f216d4cf524c7
Marco <formante.marco@gmail.com>
2026-02-11 22:47:03 +0000

    get user info from cookies(), add Alert provider and user Provider

--- frontend/app/(admin)/layout.tsx
+++ frontend/app/(admin)/layout.tsx
@@ -1,17 +1,28 @@
 import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
 import "../assets/sass/globals.css"
+import { cookies } from "next/headers";
+import { UserProvider } from "../context/UserContext";
+import { AlertProvider } from "../context/AlertContext";
 
 export default async function AdminLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+ const userInfo =(await cookies()).get("user_info")?.value
+  const user = userInfo ? JSON.parse(userInfo) : null
+
   return (
+     <UserProvider user={user}>
       <div className="bg-[#F9FAFB] min-h-screen">
-        <Header/>
-            {children}
-        <Footer/>
+        <Header />
+        <AlertProvider>
+          {children}
+        </AlertProvider>
+        <Footer />
       </div>
+    </UserProvider>
   );
 }
