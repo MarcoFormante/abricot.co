fd7d1c918b6883b0a199d7cbb36b2f550250fbad
Marco <formante.marco@gmail.com>
2026-02-11 22:47:58 +0000

    change way to get userprofile and add to props

--- frontend/app/(admin)/mon-compte/page.tsx
+++ frontend/app/(admin)/mon-compte/page.tsx
@@ -2,11 +2,11 @@ import { getUserProfile } from "@/app/actions/auth";
 import { UserForm } from "@/app/components/monCompte/UserForm";
 
 export default async function MonCompte(){
-    const profile = await getUserProfile()
-
+    const userProfile = await getUserProfile()
+    
     return (
         <main className="px-25 pb-[78px]">
-            <UserForm user={profile?.data?.user}/>
+            <UserForm user={userProfile.user}/>
         </main>
     )
 }
