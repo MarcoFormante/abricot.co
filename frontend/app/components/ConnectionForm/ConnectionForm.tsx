7ba84348f6b7f4723c238a041202435e3d099c94
Marco <formante.marco@gmail.com>
2026-02-11 22:41:25 +0000

    add comment for function, handle success and error response

--- frontend/app/components/ConnectionForm/ConnectionForm.tsx
+++ frontend/app/components/ConnectionForm/ConnectionForm.tsx
@@ -1,15 +1,22 @@
 'use client'
 
 import { login, register } from "@/app/actions/auth";
-import { Button } from "../Button/Button";
 import { Form } from "./Form/Form";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { useAlert } from "@/app/context/AlertContext";
+import { Submit } from "../Submit/Submit";
 
 export function ConnectionForm({isRegisterPage}:{isRegisterPage:boolean}){
-    const [formErrors,setFormErrors] = useState<[string] | string>("")
+    const [formErrors,setFormErrors] = useState<string[] | string>("")
     const router = useRouter()
+    const setAlert = useAlert()
 
+    /**
+     * Handle Login and Register action
+     * @param e Form Event
+     * @returns 
+     */
     const submitForm = async (e:React.FormEvent<HTMLFormElement>)=>{
         e.preventDefault()
         const formData = new FormData(e.currentTarget)
@@ -19,23 +26,26 @@ export function ConnectionForm({isRegisterPage}:{isRegisterPage:boolean}){
             return
         }
         
-        let response
-
         if (!isRegisterPage) {  
-            response = await login(formData)
-            if (response?.status !== 200) {
-                setFormErrors(response?.message || "Une erreur est survenue")
+            const response = await login(formData)
+            if (!response?.success) {
+                setFormErrors(response?.errorMessage || "Une erreur est survenue")
             }else{
+                setAlert({type:"success",message:response?.message})
                 router.push("/dashboard")
             }
         }else{
-            response = await register(formData)
-            if (response?.success) {
-                 router.push("/mon-compte")
+           const response = await register(formData)
+            if (!response?.success) {
+                setFormErrors(response?.errorMessage || "Une erreur est survenue")
+                
+            }else{
+                setAlert({type:"success",message:response?.message})
+                router.push("/mon-compte")
             }
         }
     }
-
+    
 
     return (
         <div className="w-70.5 flex flex-col gap-7.5">
@@ -43,13 +53,12 @@ export function ConnectionForm({isRegisterPage}:{isRegisterPage:boolean}){
             <div>
                  <div className="flex flex-col pb-3  text-red-700">
                         {formErrors && Array.isArray(formErrors)
-                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
                         ? formErrors.map((err:any,i:number)=> <p key={`error${i}`}>{err.message}</p>)
                         : <p>{formErrors}</p>
                     }
                     </div>
                 <Form onSubmit={submitForm} isRegisterPage={isRegisterPage}>
-                    <Button label={isRegisterPage ?"S’inscrire": "Se connecter"} type="btn-softBlack"/>
+                    <Submit label={isRegisterPage ?"S’inscrire": "Se connecter"} type="btn-softBlack"/>
                 </Form>
             </div>
         </div>
