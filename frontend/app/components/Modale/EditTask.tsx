b6b4e4c969a5132f4efb752a0ffb20c0c0756316
Marco <formante.marco@gmail.com>
2026-02-11 16:35:16 +0000

    add submit component inside the form, edit modale names,eedit onDateChange function

--- frontend/app/components/Modale/EditTask.tsx
+++ frontend/app/components/Modale/EditTask.tsx
@@ -1,107 +1,131 @@
+'use client'
+import { Input } from '../Input/Input';
+import { Tag } from '../Dashboard/Tasks/Tag';
+import { useState } from 'react';
+import { editTask } from '@/app/actions/task';
+import { useRouter } from "next/navigation";
+import { Submit } from '../Submit/Submit';
 
+export function EditTask({task,members,setShowModale}:
+    {
+        task:any,
+        members:any,
+        setShowModale:React.Dispatch<React.SetStateAction<{type:string}>>
+    }){
+        
+    const [newDate,setNewDate] = useState(task?.dueDate)
+    const [selectedUsers,setSelectedUser] = useState<any[]>(task?.assignees?.map((a:any)=> a.user.id))
+    const router = useRouter()
 
-import { useState } from "react";
-import { Button } from "../Button/Button";
-import { Tag } from "../Dashboard/Tasks/Tag";
-import { Input } from "../Input/Input";
+    async function onSubmit(event: React.FormEvent) {
+        event.preventDefault()
+        const formData = new FormData(event.target as HTMLFormElement)
+        const title = formData.get('title')
+        const desc = formData.get('desc')
+        const date = formData.get('date')
 
-const fakeData = {
-    id:"1523",
-    title:"Authentification JWT",
-    desc: "Implémenter le système d'authentification avec tokens JWT",
-    date:"2026/03/09",
-    collabs : [
-        {
-        id:"225",
-        name:"Ciccio Banana" 
-        },
-        {
-            id:"335",
-            name:"Stefano Bellavita"
+        if (!title && !desc && !date) {
+            return
         }
-    ],
-    status:"to-do"
-}
+        
+        const formValues = {
+            title: formData.get('title'),
+            description: formData.get('desc'),
+            dueDate: new Date(newDate).toISOString(),
+            assigneeIds: selectedUsers,
+            status: formData.get('status[]') || task.status,
+            projectId:task.projectId,
+            taskId:task.id
+        }
+        const response = await editTask(formValues)
+        
+        if (response.success) {
+            setShowModale({type:""})
+            document.body.style.overflowY = "visible"
+            router.refresh()
+        }
+    }
 
-export function EditTask(){
-     const [newDate,setNewDate] = useState(new Date(fakeData.date).toLocaleDateString("fr-FR",{month:"long",day:"numeric",year:"numeric"}) ?? "")
-    
-        function onSubmit(event: React.FormEvent) {
-            event.preventDefault()
-            console.log(event);
-            
-            const formData = new FormData(event.target as HTMLFormElement)
-            const title = formData.get('title')
-            const desc = formData.get('desc')
-            const date = formData.get('date')
-    
-            if (!title && !desc && !date) {
-                return
-            }
-    
-            const formValues = {
-                title: formData.get('title'),
-                desc: formData.get('desc'),
-                date: formData.get('date'),
-                collabs: formData.get('collaborators'),
-                status: formData.get('status[]')
-            }
-    
-            console.log(formValues);
-        }
-    
-        const onDateChange = (value:string)=>{
-            const date = new Date(value)
-            const isoDate = date.toLocaleDateString("fr-FR",{
-                month:"long",day:"numeric",year:"numeric"
-            })
-            setNewDate(isoDate)
-        }
+
+    const onDateChange = (value:string)=>{
+        const date = new Date(value)
+        
+        const isoDate = new Date(date).toISOString()
+
+        setNewDate(isoDate)
+    }
+  
+  
+      const onSelectChange = (value:string) => {
+          if (!value) {
+              return
+          }
+          if (!selectedUsers.includes(value)) {
+              setSelectedUser(prev => [...prev,value])
+          }else{
+              setSelectedUser(selectedUsers.filter((email)=> email !== value))
+          }
+      }
 
 
     return (
         <div className="flex flex-col gap-[46px]">
-            <h5 className="font-semibold text-[24px] text-[#1F1F1F]">Modifier</h5>  
-                <form onSubmit={onSubmit} className='flex flex-col gap-[24px]'>
-                    <Input type='text' name='title' label='Title*' gap='6px' required value={fakeData.title ?? ""}/>
-                    <Input type='text' name='desc' label='Description*' gap='6px' required value={fakeData.desc ?? ""}/>
-                    <div className='relative'>
-                        <Input type={"date"} name='date' label='Échéance*' required  onChange={(e) => onDateChange(e.target.value)} value={"2026-03-26"} />
-                        <input name='isoDate' id='isoDate' disabled className='absolute top-[50%] left-0.5 h-max rounded-sm bg-[#FFFFFF]  border-[#E5E7EB] pl-[17px] w-[80%] focus:outline-0' type={"text"} value={newDate || fakeData.date }/>
+            <h5 className="font-semibold text-[24px] text-[#1F1F1F]">Modifier</h5>
+
+            <form onSubmit={onSubmit} className='flex flex-col gap-[24px]'>
+                <Input type='text' name='title' label='Title*' gap='6px' value={task.title ?? ""} required/>
+                <Input type='text' name='desc' label='Description*' gap='6px' value={task.description ?? ""} required/>
+                <div className='relative'>
+                    <Input type={"date"} name='date' label='Échéance*'  onChange={(e) => onDateChange(e.target.value)} value={new Date(newDate).toString()}  />
+                    <input name='isoDate' id='isoDate' disabled className='absolute top-[50%] left-0.5 h-max rounded-sm bg-[#FFFFFF]  border-[#E5E7EB] pl-1.5 w-[80%] focus:outline-0' type={"text"} value={new Date(newDate).toLocaleDateString("fr-FR",{day:"numeric",month:"long",year:"numeric"})}/>
+                </div>
+                
+                <div className='relative'>
+                    <label htmlFor="collaborators" className='text-[14px]'>Assigné à :</label>
+                    <div className="absolute top-[50%] pointer-events-none left-[17px] text-[#6B7280] ">{selectedUsers?.length ? selectedUsers.length + " collaborateurs" : "Choisir un ou plusieurs collaborateurs"}</div>
+                    <select value="" onChange={(e)=>onSelectChange(e.target.value)} name="collaborators" id="collaborators" className="select-container h-[53px]  pl-[17px] w-full text-[14px]  rounded-sm bg-[#FFFFFF] border border-[#E5E7EB]  pl-1.5 text-[#6B7280]" >
+                        <option value=""></option>
+                        {
+                             (members && members?.length) && 
+                                members?.map((m:any)=> 
+                                <option 
+                                    data-selected={selectedUsers?.includes(m.user.id)}  
+                                    key={m.id} 
+                                    value={m.user.id} 
+                                    className="text-[#6B7280]"
+                                    >{m.user.name}
+                                </option> 
+                            )
+                        }
+
+                       
+                    </select>   
+                </div>
+
+                <div>
+                    <span className='text-[14px]'>Statut :</span>
+                    <div className='flex items-center gap-[8px] mt-[15px]'>
+                        <input type="radio" name="status[]" id="to-do" value={"TODO"}  defaultChecked={task.status === "TODO"}/>
+                        <label htmlFor="to-do">
+                            <Tag type='TODO'/>
+                        </label>
+                        
+                        <input type="radio" name="status[]" id="in-progress" value={"IN_PROGRESS"} defaultChecked={task.status === "IN_PROGRESS"} />
+                        <label htmlFor="in-progress">
+                            <Tag type='IN_PROGRESS'/>
+                        </label>
+
+                        <input type="radio" name="status[]" id="done" value={"DONE"} defaultChecked={task.status === "DONE"} />
+                        <label htmlFor="done">
+                            <Tag type='DONE'/>
+                        </label>
                     </div>
-                    
-                    <div>
-                        <label htmlFor="collaborators" className='text-[14px]'>Assigné à :</label>
-                        <select multiple name="collaborators" id="collaborators" className="select-container h-[53px]  pl-[17px] w-full text-[14px]  rounded-sm bg-[#FFFFFF] border border-[#E5E7EB]  pl-1.5 text-[#6B7280]" defaultValue={""}>
-                            <option value="" className="text-[#6B7280] ">{fakeData.collabs.length ? `${fakeData.collabs.length < 2 ? "1 collaborateur" : fakeData.collabs.length + " collaborateurs" } ` : "Choisir un ou plusieurs collaborateurs"}</option>
-                            {fakeData.collabs.map((c) => <option style={{background:"orange"}} key={c.id} value={c.id} className="text-[#6B7280]">{c.name}</option> ) }
-                        </select>
-                    </div>
-        
-                    <div>
-                        <span className='text-[14px]'>Statut :</span>
-                        <div className='flex items-center gap-[8px] mt-[15px]'>
-                            <input type="radio" name="status[]" id="to-do" value={"to-do"} checked={fakeData.status === "to-do"} />
-                            <label htmlFor="to-do">
-                                <Tag type='TODO'/>
-                            </label>
-                            
-                            <input type="radio" name="status[]" id="in-progress" value={"in-progress"} checked={fakeData.status === "in-progress"} />
-                            <label htmlFor="in-progress">
-                                <Tag type='IN_PROGRESS'/>
-                            </label>
-        
-                         <input type="radio" name="status[]" id="done" value={"done"} checked={fakeData.status === "done"} />
-                            <label htmlFor="done">
-                                <Tag type='DONE'/>
-                            </label>
-                        </div>
-                    </div>
-        
-                    <div className='w-[181px] h-[50px] mt-[32px]'>
-                        <Button type={"btn-grey"} label={"Enregistrer"} />
-                    </div>
-                </form>
+                </div>
+
+                <div className='w-[181px] h-[50px] mt-[32px]'>
+                     <Submit type={"btn-grey"} label={"Modifier"}/>
+                </div>
+            </form>
         </div>
     )
 }
