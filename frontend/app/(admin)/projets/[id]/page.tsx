15b41f90ca8806749a8c4fda2e569adbd276ddf1
Marco <formante.marco@gmail.com>
2026-02-11 22:48:45 +0000

    get user info from cookies()

--- frontend/app/(admin)/projets/[id]/page.tsx
+++ frontend/app/(admin)/projets/[id]/page.tsx
@@ -1,26 +1,44 @@
+
+
 import { ProjectHeader } from "@/app/components/SingleProject/ProjectHeader";
 import { ProjectContributors } from "@/app/components/SingleProject/ProjectContributors";
 import { ProjectTasks } from "@/app/components/SingleProject/ProjectTasks/ProjectTasks";
 import {getProjectByID} from "@/app/actions/project";
+import { cookies } from "next/headers";
 
 export default async function SingleProject({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
-
   const param = await params;
-  const project = await getProjectByID(param.id)
+  const project = await getProjectByID(param.id);
+
+  const userInfo =(await cookies()).get("user_info")?.value
+  const userParsed = JSON.parse(userInfo as string)
   
   return (
     <main className="pb-[78px]">
-    {  project && 
-      <>
-        <ProjectHeader id={param.id} name={project.data.name} description={project.data.description}/> 
-        <ProjectContributors ownerName={project.data.owner.name} members={project.data.members}/>
-        <ProjectTasks tasks={project.tasks}/>
-       </>
-    }
+      {project && (
+        <>
+            <ProjectHeader 
+              id={param.id} 
+              name={project.data.name} 
+              description={project.data.description} 
+              members={project.data.members}
+              isUserProject={project.data.owner.id === userParsed.id}
+            /> 
+            <ProjectContributors 
+              members={project.data.members}
+              owner={project.data.owner}
+            />
+            <ProjectTasks 
+              tasks={project.tasks} 
+              projectMembers={project.data.members} 
+              isUserProject={project.data.owner.id === userParsed.id}
+            />
+          </>
+      )}
     </main>
   );
 }
