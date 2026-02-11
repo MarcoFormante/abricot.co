92f15dc239f91f51fe1b83c5b3e35eb4af7fd44e
Marco <formante.marco@gmail.com>
2026-02-11 16:01:51 +0000

    create Submit component to handle form submition

---
+++ frontend/app/components/Submit/Submit.tsx
@@ -0,0 +1,11 @@
+import { useFormStatus } from "react-dom";
+import { Button } from "../Button/Button";
+import { ButtonType } from '../../types/globals';
+
+export function Submit(props:ButtonType){
+     const status = useFormStatus()
+
+    return (
+        <Button {...props} label={status.pending ? "En cours" : props?.label}  disabled={status?.pending || false} />
+    )
+}
