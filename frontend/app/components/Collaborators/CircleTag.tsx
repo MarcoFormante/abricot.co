8ef7ca9da68a8349c589d59f15c217ee00e5ba76
Marco <formante.marco@gmail.com>
2026-02-11 22:42:03 +0000

    add title prop

--- frontend/app/components/Collaborators/CircleTag.tsx
+++ frontend/app/components/Collaborators/CircleTag.tsx
@@ -6,7 +6,7 @@ export function CircleTag({name,isOwner,classname,rightValue}:{
 }){
  
     return (
-        <span data-right={rightValue} className={`circleTag uppercase rounded-full bg-[${isOwner === true ? "#FFE8D9" : "#E5E7EB"}] text-[10px] text-[#0F0F0F] px-[5px] py-[9px] w-[27px] h-[27px] flex justify-center items-center ${classname ?? ""}`}>
+        <span title={name} data-right={rightValue} className={`circleTag uppercase rounded-full bg-[${isOwner === true ? "#FFE8D9" : "#E5E7EB"}] text-[10px] text-[#0F0F0F] px-[5px] py-[9px] w-[27px] h-[27px] flex justify-center items-center ${classname ?? ""}`}>
             {name?.split(" ").map((w:string) => w.at(0) )}
         </span>
     )
