2bee8448b0e8cb114dd59eb6596be8177851ea04
Marco <formante.marco@gmail.com>
2026-02-11 16:14:05 +0000

    delete comments

--- frontend/app/inscription/page.tsx
+++ frontend/app/inscription/page.tsx
@@ -2,20 +2,6 @@ import { SignInUp } from "../components/SignInUp/SignInUp";
 
 export default function Inscription() {
 
-  //   const req = await fetch("http://localhost:8000/auth/login",{
-  //   method:"POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body:JSON.stringify({
-  //     email: "alice@example.com",
-  //     password: "password123",
-  //   }),
-  // })
-
-  // const res = await req.json()
-  
-
   return (
     <main>
       <SignInUp isRegisterPage={true}/>
