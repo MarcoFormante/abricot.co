642a1847f5ef25c3c0fba56279fb0afa4ddbb1ed
Marco <formante.marco@gmail.com>
2026-02-11 15:59:50 +0000

    user provider to share users in all components

---
+++ frontend/app/context/UserContext.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { createContext,useContext } from "react"
+
+const UserContext = createContext<any>(null)
+
+export function UserProvider({children,user}:{
+    children:React.ReactNode,
+    user:any
+}){
+    return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+}
+
+export const useUser = () => useContext(UserContext)
