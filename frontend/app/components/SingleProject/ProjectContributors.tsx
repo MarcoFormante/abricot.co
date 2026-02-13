5c53b553b413a3261f5ff37dbe6ccfe1fe699776
Marco <formante.marco@gmail.com>
2026-02-13 19:53:05 +0000

    edit contributors order, first the owner

--- frontend/app/components/SingleProject/ProjectContributors.tsx
+++ frontend/app/components/SingleProject/ProjectContributors.tsx
@@ -6,7 +6,7 @@ export function ProjectContributors({members,owner}:{
   members:MemberInterface[],
   owner:UserInterface
 }){
-  
+
     return (
       <section className="px-[112px] max-lg:px-5 max-sm:px-2">
         <div className="bg-[#F3F4F6] mt-[49px] py-[21px] px-[50px] rounded-[10px] flex justify-between max-sm:px-2 max-[1200px]:flex-col max-md:gap-2 max-lg:px-5" >
@@ -18,21 +18,24 @@ export function ProjectContributors({members,owner}:{
           </div>
           <div>
             <div className="flex items-center justify-end flex-wrap gap-y-3 max-w-[700px] max-[1200px]:justify-start max-[1200px]:mt-2">
-            {
-              members && members.map((m) => 
+                <div className="flex">
+                    <CircleTag name={owner.name} isOwner={true}/>
+                    <NameTag name={owner.name} isOwner={true}/>
+                </div>
+
+             { members && members.map((m) => 
                 {
-                  return (
+                  return m.user.id !== owner.id && (
                     <div key={m.id} className="flex">
                       <CircleTag name={m.user.name} isOwner={m.user.id === owner.id }/>
                       <NameTag name={m.user.name} isOwner={m.user.id === owner.id}/>
                     </div>
                   )
                 }
-              )
-            } 
+              )}
+               </div>
             </div>
           </div>
-        </div>
       </section>
     )
 }
