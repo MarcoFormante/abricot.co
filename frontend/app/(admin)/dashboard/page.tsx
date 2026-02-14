3141dd85f2e62866b157e2609d32de6e649fe0ed
Marco <formante.marco@gmail.com>
2026-02-14 10:29:44 +0000

    bug fixed : userInfo not parsed

--- frontend/app/(admin)/dashboard/page.tsx
+++ frontend/app/(admin)/dashboard/page.tsx
@@ -13,8 +13,8 @@ export const metadata: Metadata = {
 
 export default async function Dashboard() {
 
-    const userInfo = (await cookies()).get("user_info")?.name
-    // const userParsed = JSON.parse(userInfo as string)
+    const userInfo = (await cookies()).get("user_info")?.value
+    const userParsed = JSON.parse(userInfo as string)
 
     const response = await getDashboardTasks()
     const tasks = response?.tasks
@@ -23,7 +23,7 @@ export default async function Dashboard() {
     return (
         <main className="pt-[40px] px-5 lg:px-25 pb-[78px] max-sm:px-2">
             <div className="flex flex-col justify-center gap-8 lg:flex-row lg:justify-between">
-                <AdminMainTitle title={"Tableau de bord"} text={`Bonjour ${userInfo}, voici un aperçu de vos projets et tâches`}/>
+                <AdminMainTitle title={"Tableau de bord"} text={`Bonjour ${userParsed?.name}, voici un aperçu de vos projets et tâches`}/>
                 <CreateProjectButton />
             </div>
 
