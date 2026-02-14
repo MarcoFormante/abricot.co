e5c0cb64af0af756796ca9961db31a2a5eb3a07e
Marco <formante.marco@gmail.com>
2026-02-14 10:07:58 +0000

    check for atuh_token and user_profile data and redirect to login page if not

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -7,9 +7,9 @@ export async function proxy(request: NextRequest) {
   const userInfo = request.cookies.get("user_info")
   const authPaths = ['/','/inscription']
 
-    if ((!userInfo && !token) && !authPaths.includes(request.nextUrl.pathname)) {
+  if ((!userInfo || !token) && !authPaths.includes(request.nextUrl.pathname)) {
        return NextResponse.redirect(new URL('/', request.url))
-    }
+  }
   
   const headers = new Headers(request.headers);
   headers.set("x-current-path", request.nextUrl.pathname);
